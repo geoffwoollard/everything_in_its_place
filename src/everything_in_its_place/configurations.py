77727e0f29c9e2d7545fa6db5e4db215958297ff
Geoffrey Woollard <geoffwoollard@gmail.com>
2024-07-02 12:25:45 -0400

    hydra

--- src/everything_in_its_place/configurations.py
+++ src/everything_in_its_place/configurations.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 @dataclass
 class BaseFunctionsConfig:
@@ -9,10 +10,12 @@ class BaseFunctionsConfig:
 class CastingConfig:
     cast_output_after_computation: bool = False
     cast_input_before_computation: bool = False
-    
+
 @dataclass
 class AddConfig(BaseFunctionsConfig):
     cast_to_int: bool = CastingConfig.cast_output_after_computation
     operation: str = "add"
+    input1: List[float] = None
+    input2: List[float] = None
 
 
--- src/everything_in_its_place/run.py
+++ src/everything_in_its_place/run.py
@@ -1,23 +1,13 @@
+import hydra
 from omegaconf import OmegaConf
-import argparse
 
 from everything_in_its_place.higher_level_functions import add_loop
 from everything_in_its_place.configurations import AddConfig
 
-def main():
-    # Step 1: Set up argparse
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument('--config_fname', type=str, help='Configuration file name', required=True)
-    args = parser.parse_args()
-
-    # Step 2: Use the provided argument for the configuration file name
-    conf_from_yaml = OmegaConf.load(args.config_fname)
-
-    # Step 3: Validate the configuration
-    validated_conf = OmegaConf.merge(AddConfig, conf_from_yaml)
-
-    list1 = [1.0, 2.0, 3.0]
-    list2 = [10, 20, 30]
+@hydra.main(version_base=None, config_path='configs', config_name='config_add')
+def main(validated_conf: AddConfig):
+    list1 = validated_conf.input1
+    list2 = validated_conf.input2
     n = 3
     result = add_loop(list1, list2, n)
     if validated_conf.cast_to_int:
