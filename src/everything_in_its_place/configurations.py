60e230b8bc688a038ee2570631b329f53a839a5e
Geoffrey Woollard <geoffwoollard@gmail.com>
2024-07-02 11:44:16 -0400

    add with field from another dataclass with . syntax. notebook example with failed validation

--- src/everything_in_its_place/configurations.py
+++ src/everything_in_its_place/configurations.py
@@ -5,8 +5,14 @@ class BaseFunctionsConfig:
     dtype: str = 'float64'
     operation: str = None
     
-
+@dataclass
+class CastingConfig:
+    cast_output_after_computation: bool = False
+    cast_input_before_computation: bool = False
+    
 @dataclass
 class AddConfig(BaseFunctionsConfig):
-    cast_to_int: bool = False
+    cast_to_int: bool = CastingConfig.cast_output_after_computation
     operation: str = "add"
+
+
